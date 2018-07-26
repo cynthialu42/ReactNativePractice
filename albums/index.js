786a4a90b5afdc706dda613ff0cebd58bd1a9204
cynthialu42 <clu2334@gmail.com>
2018-07-26 15:45:15 -0500

    album list

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,16 @@
 // import library to create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 //create a component
 const App = () => (
-    <Text>Some Text</Text>
+    <View>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
+    </View>
+
 );
 
 // Render it to the device
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+// import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+    componentDidMount() {
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(res => res.json())
+            .then(resData => this.setState({ albums: resData }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => {
+            return <AlbumDetail key={album.title} album={album} />;
+        });
+    }
+    render() {
+        console.log('state is', this.state);
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }  
+}
+
+export default AlbumList;
